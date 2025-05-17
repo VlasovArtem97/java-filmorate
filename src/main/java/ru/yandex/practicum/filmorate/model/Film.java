74d78cc53f94b82fbde2a12a4afda64eb0cb4b48
VlasovArtem <Yukionn@yandex.ru>
2025-05-17 21:30:35 +0300

    3. Создание проекта - filmorate (Прохождение автотестов № 2)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
