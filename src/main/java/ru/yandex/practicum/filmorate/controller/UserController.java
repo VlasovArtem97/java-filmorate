c0069126e1f2d89a1e1e4eb5dabccacda3fba13b
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-07-15 18:08:13 +0500

    Added get method "/{id}/friends"

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import java.util.Collections;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -42,17 +40,9 @@ public class UserController {
         userService.unfriending(userId, friendId);
     }
 
-    /*@GetMapping("/{id}/friends")
-    public Collection<User> friendList(@Positive @PathVariable("id") Long userId) {
-        return userService.friendsList(userId);
-    }*/
     @GetMapping("/{id}/friends")
     public Collection<User> friendList(@Positive @PathVariable("id") Long userId) {
-        try {
-            return userService.friendsList(userId);
-        } catch (NotFoundException e) {
-            return Collections.emptyList();
-        }
+        return userService.friendsList(userId);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
